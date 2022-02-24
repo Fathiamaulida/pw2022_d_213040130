512e72e2d79e7dee7b49f24660a07a9017d6ba3c
fathiamaulida <fathiamaulida12345@gmail.com>
2022-02-24 11:42:11 +0700

    mengerjakan tugas pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,54 @@
+<?php 
+//perbandingan 
+// < , > , <= , >= , == , != 
+
+// penggunaan variabel
+//tempat penampungan nilai
+// tidak boleh mnegandung spasi 
+// tidak menampung nilai
+// tidak boleh mnegandung angka, tidak boeh di awal 
+// snake_case : $nama_depan
+// camelCase : namaDepanMahasiswa
+// kebab-case : nama-depan-mahasiswa
+// contoh
+$nama = 'fathia' ;
+echo $nama;
+echo ""
+//penugasan /assigment
+// =,+=, -=, *=, /=,%= 
+$a = 10;
+$a = 20;
+$a = 30;
+echo $a;
+echo "<hr>";
+echo 10 + "10" + 10;
+echo "<hr>";
+//operator increment & Decrement
+// ++, -- (dipake untuk luping)
+$b = 10;
+$b--;
+echo $b++;
+echo "<br>"
+
+//operator identitas (menambahkan apa yang kita pelajari di perbandingan)
+// ===,!==
+// true and false
+// jika ruas kiri angka ruas kanan strings maka false 
+
+ echo 10 === 20;
+ echo 10 === 10; 
+ echo 10 === "10";
+
+ <script>
+ prompt('masukkan umur anda ')
+ </script>
+
+` back tick
+~ tilde
+# pound
+^ caret 
+| pipe
+\ backslash
+/ slash 
+
+?>
