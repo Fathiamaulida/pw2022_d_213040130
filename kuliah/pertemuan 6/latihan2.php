a8e752bf6d7c612dd4eba9679b19896efbbc0ec5
fathiamaulida <fathiamaulida12345@gmail.com>
2022-04-18 21:12:06 +0700

    menyelesaikan tugas pertemuan ke 6

---
+++ kuliah/pertemuan 6/latihan2.php
@@ -0,0 +1,21 @@
+<?php
+$mahasiswa = ["Fathia Maulida","213040130","Fathiamaulida145@gmail.com","Teknik informatika"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <ul>
+    <li>Fathia Maulida</li>
+    <li>213040130</li>
+    <li>Fathiamaulida145@gmail.com</li>
+    <li>Teknik informatika</li>
+    </ul>
+</body>
+</html>
